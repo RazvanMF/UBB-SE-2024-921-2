224955d4468e55f725c4ca02a4597ef7f5798d2b
Rares Bozga <darwj10011@gmail.com>
2024-05-19 17:26:53 +0300

    ReviewService now uses the ReviewsController endpoints.

--- application-namespacegpt/NamespaceGPT.Business/Services/ReviewService.cs
+++ application-namespacegpt/NamespaceGPT.Business/Services/ReviewService.cs
@@ -14,37 +14,158 @@ namespace NamespaceGPT.Business.Services
 
         public int AddReview(Review review)
         {
-            return reviewRepository.AddReview(review);
+            try
+            {
+                HttpClient client = new HttpClient();
+                StringContent content = new StringContent(JsonConvert.SerializeObject(review), Encoding.UTF8, "application/json");
+                content.GetType();
+
+                HttpResponseMessage response = Task.Run(() => client.PostAsync("https://localhost:7040/api/reviews", content)).GetAwaiter().GetResult();
+                response.EnsureSuccessStatusCode();  // error-prone
+                string responseBody = Task.Run(() => response.Content.ReadAsStringAsync()).GetAwaiter().GetResult();
+                Review? result = JsonConvert.DeserializeObject<Review>(responseBody);
+                if (result == null)
+                {
+                    throw new Exception("???");
+                }
+
+                return result.Id;
+            }
+            catch
+            {
+                return -1;
+            }
+
+            // return reviewRepository.AddReview(review);
         }
 
         public bool DeleteReview(int id)
         {
-            return reviewRepository.DeleteReview(id);
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = Task.Run(() => client.DeleteAsync("https://localhost:7040/api/reviews/" + id)).GetAwaiter().GetResult();
+                response.EnsureSuccessStatusCode();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            //return reviewRepository.DeleteReview(id);
         }
 
         public IEnumerable<Review> GetAllReviews()
         {
-            return reviewRepository.GetAllReviews();
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = Task.Run(() => client.GetAsync("https://localhost:7040/api/reviews")).GetAwaiter().GetResult();
+                response.EnsureSuccessStatusCode();  // error-prone
+                string responseBody = Task.Run(() => response.Content.ReadAsStringAsync()).GetAwaiter().GetResult();
+                List<Review>? result = JsonConvert.DeserializeObject<List<Review>>(responseBody);
+                if (result == null)
+                {
+                    throw new Exception("???");
+                }
+                return result;
+            }
+            catch
+            {
+                return null;
+            }
+
+            //return reviewRepository.GetAllReviews();
         }
 
         public Review? GetReview(int id)
         {
-            return reviewRepository.GetReview(id);
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = Task.Run(() => client.GetAsync("https://localhost:7040/api/reviews" + id)).GetAwaiter().GetResult();
+                response.EnsureSuccessStatusCode();  // error-prone
+                string responseBody = Task.Run(() => response.Content.ReadAsStringAsync()).GetAwaiter().GetResult();
+                List<Review>? result = JsonConvert.DeserializeObject<List<Review>>(responseBody);
+                if (result == null)
+                {
+                    throw new Exception("???");
+                }
+                return result[0]; // TODO: dubious endpoint?
+            }
+            catch
+            {
+                return null;
+            }
+            //return reviewRepository.GetReview(id);
         }
 
         public IEnumerable<Review> GetReviewsForProduct(int productId)
         {
-            return reviewRepository.GetReviewsForProduct(productId);
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = Task.Run(() => client.GetAsync("https://localhost:7040/api/reviews")).GetAwaiter().GetResult();
+                response.EnsureSuccessStatusCode();  // error-prone
+                string responseBody = Task.Run(() => response.Content.ReadAsStringAsync()).GetAwaiter().GetResult();
+                List<Review>? result = JsonConvert.DeserializeObject<List<Review>>(responseBody);
+                if (result == null)
+                {
+                    throw new Exception("???");
+                }
+                // TODO: should really have its own end-point, this is slow
+                return result.Where(el => el.ProductId == productId);
+            }
+            catch
+            {
+                return null;
+            }
+
+            //return reviewRepository.GetReviewsForProduct(productId);
         }
 
         public IEnumerable<Review> GetReviewsFromUser(int userId)
         {
-            return reviewRepository.GetReviewsFromUser(userId);
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = Task.Run(() => client.GetAsync("https://localhost:7040/api/reviews")).GetAwaiter().GetResult();
+                response.EnsureSuccessStatusCode();  // error-prone
+                string responseBody = Task.Run(() => response.Content.ReadAsStringAsync()).GetAwaiter().GetResult();
+                List<Review>? result = JsonConvert.DeserializeObject<List<Review>>(responseBody);
+                if (result == null)
+                {
+                    throw new Exception("???");
+                }
+                // TODO: should really have its own end-point, this is slow
+                return result.Where(el => el.UserId == userId);
+            }
+            catch
+            {
+                return null;
+            }
+
+            //return reviewRepository.GetReviewsFromUser(userId);
         }
 
         public bool UpdateReview(int id, Review review)
         {
-            return reviewRepository.UpdateReview(id, review);
+            try
+            {
+                HttpClient client = new HttpClient();
+                StringContent content = new StringContent(JsonConvert.SerializeObject(review), Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = Task.Run(() => client.PutAsync("https://localhost:7040/api/reviews" + id, content)).GetAwaiter().GetResult();
+                response.EnsureSuccessStatusCode();  // error-prone
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+            //return reviewRepository.UpdateReview(id, review);
         }
     }
 }
