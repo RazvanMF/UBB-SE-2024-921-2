1b4d8088dbff8914766d061e9dcdec04ff8a2022
Razvan <81688012+RazvanMF@users.noreply.github.com>
2024-05-19 12:27:10 +0300

    deleted unused imports

--- application-namespacegpt/NamespaceGPT.WPF/ProductPages/ProductPage.xaml.cs
+++ application-namespacegpt/NamespaceGPT.WPF/ProductPages/ProductPage.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using NamespaceGPT.Api.Controllers;
 using NamespaceGPT.Data.Models;
 
