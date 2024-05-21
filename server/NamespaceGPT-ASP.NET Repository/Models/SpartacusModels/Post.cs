b5b5441887ece10139075ef209ff08fb6bd56578
buian-dragos <dragos.buian@stud.ubbcluj.ro>
2024-05-21 21:57:04 +0300

    Models Spartacus

---
+++ server/NamespaceGPT-ASP.NET Repository/Models/SpartacusModels/Post.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NamespaceGPT_ASP.NET_Repository.Models.SpartacusModels
+{
+    public class Post
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int NumberOfLikes { get; set; } = 0;
+
+        [Required]
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        [Required]
+        [MaxLength(256)]
+        public string ImagePath { get; set; }
+
+        [Required]
+        [MaxLength(512)]
+        public string Caption { get; set; }
+
+        [ForeignKey("Comment")]
+        [Required]
+        public List<int> CommentIds { get; set; } = new List<int>();
+    }
+}
