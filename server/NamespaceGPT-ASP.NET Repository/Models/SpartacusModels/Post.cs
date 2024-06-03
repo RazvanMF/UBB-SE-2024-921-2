e7baf58008d69a918105c01ef8e2c18962eed517
MadalinCernat <madalin.cernat02@gmail.com>
2024-06-04 00:53:20 +0300

    added migration to unmap comment ids

--- server/NamespaceGPT-ASP.NET Repository/Models/SpartacusModels/Post.cs
+++ server/NamespaceGPT-ASP.NET Repository/Models/SpartacusModels/Post.cs
@@ -25,10 +25,10 @@ namespace NamespaceGPT_ASP.NET_Repository.Models.SpartacusModels
         [MaxLength(512)]
         public string Caption { get; set; }
 
-        [ForeignKey("Business")]
         [Required]
         public int BusinessId { get; set; }
 
+        [NotMapped]
         public List<int> CommentIds { get; set; } = new List<int>();
     }
 }
